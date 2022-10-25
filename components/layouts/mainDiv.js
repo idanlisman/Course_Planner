9982e702388c2425bc8162c870e6bfaa2d10826e
Idan Lisman <>
2022-10-25 23:43:38 +0300

    no message

--- components/layouts/mainDiv.js
+++
@@ -1,7 +0,0 @@
-import Style from "./layoutsStyle.module.css";
-
-function MainDiv(props) {
-  return <div className={Style.mainDiv}>{props.children}</div>;
-}
-
-export default MainDiv;
--- components/utils.js
+++
@@ -1,8 +0,0 @@
-const numberOfYears = 5;
-const numberOfSemesters = 3;
-const creditsPerCourse = 3;
-const uuidGenerator = (data) => {
-    return `${data.ID}-${data.Number}-${data.Semester}-${data.Year}`;
-}
-
-export {uuidGenerator, numberOfYears, numberOfSemesters, creditsPerCourse};
